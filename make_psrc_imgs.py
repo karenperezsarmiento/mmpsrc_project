5d1541a31d5f4e1db4f52c76087a8d26a3955dcb
karenperezsarmiento <kaper@sas.upenn.edu>
2023-06-26 11:29:23 -0400

    remade matched catalogs

--- make_psrc_imgs.py
+++ make_psrc_imgs.py
@@ -6,44 +6,32 @@ from astropy.wcs import WCS
 from astropy.coordinates import SkyCoord
 from astropy import units as u
 
-reduc_list = ["_2aspcmsubqm2_fitel_0f09-to-35f5Hz_qc_0p6rr_M_PdoCals_dt20_snr_iter1"]
-direc_list = ["ACT_Sources_2023_0f09-to-35f5_PCA0/"]
-code = ["20"]
-theta_1 = 3.0
-theta_2 = 5.0
+reduc_list = "_2aspcmsubqm2_fitel_0f09-to-35f5Hz_qc_0p6rr_M_PdoCals_dt20_snr_iter1"
+direc_list = "ACT_Sources_2023_0f09-to-35f5_PCA0/"
+code = "20"
+theta_1 = 2.0
+theta_2 = 6.0
 all_vlass = pd.read_csv("all_vlass_sources_in_cluster_maps_rad_6arcmin.csv")
 all_first = pd.read_csv("all_first_sources_in_cluster_maps_rad_6arcmin.csv")
 
-for k in range(len(reduc_list)):
-	psrc_found = pd.read_csv("psrc_lists/uncleaned_psrcs_sigma_"+reduc_list[k]+"_4.0sigma.csv")
-	psrc_found = psrc_found.loc[(psrc_found["theta_1"]==theta_1)&(psrc_found["theta_2"]==theta_2)]
-	paths_to_fits = np.array(pd.read_csv("reductions_lists/"+reduc_list[k]+"_files.txt",header=None))
-	matched = pd.read_csv("psrc_lists/matched_18arcsec_1and2_best1_vlass.csv")
-	matched = matched.loc[(matched["theta_1"]==theta_1)&(matched["theta_2"]==theta_2)]
-	for i in paths_to_fits:
-		cluster = i[0][2:20]
-		psrcs_cluster = psrc_found.loc[psrc_found["cluster"]==cluster]
-		matched_cluster = matched.loc[matched["cluster"]==cluster]
-		vlass_cluster = all_vlass.loc[all_vlass["cluster"]==cluster]
-		first_cluster = all_first.loc[all_first["cluster"]==cluster]
-		c_snr = "/home/scratch/cromero/MUSTANG2/Reductions/"+direc_list[k]+i[0][2:]
-		print(c_snr)
-		#c_map = re.sub("snr","map",c_snr)
-		hdu_snr = fits.open(c_snr)[0]
-		wcs = WCS(hdu_snr.header)
-		sky_vlass = SkyCoord(ra=np.array(vlass_cluster["RA_2"])*u.degree,dec=np.array(vlass_cluster["DEC_2"])*u.degree, frame="icrs")
-		sky_first = SkyCoord(ra=np.array(first_cluster["RA_2"])*u.degree,dec=np.array(first_cluster["DEC_2"])*u.degree, frame="icrs")
-		x_vlass,y_vlass = wcs.world_to_pixel(sky_vlass)
-		x_first,y_first = wcs.world_to_pixel(sky_first)
-		fig = plt.figure()
-		img_snr = hdu_snr.data
-		plt.subplot(projection=wcs)
-		plt.imshow(img_snr,origin="lower",cmap="bwr")
-		plt.title(cluster+" red "+code[k])
-		plt.scatter(np.array(psrcs_cluster["x"]),np.array(psrcs_cluster["y"]),color="none",edgecolor="black")
-		plt.scatter(np.array(matched_cluster["x"]),np.array(matched_cluster["y"]),color="none",edgecolor="green")
-		plt.scatter(x_vlass,y_vlass,color="none",marker="s",edgecolor="blue")
-		plt.scatter(x_first,y_first,color="none",marker="s",edgecolor="blue")
-		plt.savefig("psrc_img/"+cluster+"reduc_"+code[k]+".png")
-		plt.close(fig)
+psrcs_found = pd.read_csv("psrc_lists/final_cat_snr_5_3arcmin_snr_7.csv")
+matched = pd.read_csv("psrc_lists/final_matched_snr_5_3arcmin_snr_7.csv")
 
+clusters = np.unique(np.array(list(psrcs_found["cluster"])))
+for i in clusters:
+	c_snr = "/home/scratch/cromero/MUSTANG2/Reductions/"+direc_list+i+"/Jy_"+i+reduc_list+".fits"
+	hdu_snr = fits.open(c_snr)[0]
+	wcs = WCS(hdu_snr.header)
+	matched_cluster = matched.loc[matched["cluster"]==i]
+	first_cluster = all_first.loc[all_first["cluster"]==i]
+	sky_first = SkyCoord(ra=np.array(first_cluster["RA_2"])*u.degree,dec=np.array(first_cluster["DEC_2"])*u.degree,frame="icrs")
+	x_first,y_first = wcs.world_to_pixel(sky_first)
+	fig = plt.figure()
+	img_snr = hdu_snr.data
+	plt.subplot(projection=wcs)
+	plt.imshow(img_snr,origin="lower",cmap="bwr")
+	plt.title(str(i))	
+	#plt.scatter(x_first,y_first,color="none",marker="s",edgecolor="blue")
+	plt.scatter(np.array(matched_cluster["x"]),np.array(matched_cluster["y"]),color="none",edgecolor="green")	
+	plt.savefig("psrc_img/"+str(i)+"_psrc_matched_first.png")
+	plt.close(fig)
--- noise_all.py
+++ noise_all.py
@@ -4,8 +4,8 @@ from astropy.io import fits
 import pandas as pd
 import re
 
-reduc_list = ["_2aspcmsubqm2_fitel_0f09-to-35f5Hz_qc_0p6rr_M_PdoCals_dt20_snr_iter1_files.txt"]
-direc_list= ["ACT_Sources_2023_0f09-to-35f5_PCA0/"]
+reduc = "_2aspcmsubqm2_fitel_0f09-to-35f5Hz_qc_0p6rr_M_PdoCals_dt20_snr_iter1_files.txt"
+direc = "ACT_Sources_2023_0f09-to-35f5_PCA0/"
 cluster_name = []
 good_noise_area = []
 b = np.linspace(0.0,0.01,101)
@@ -16,48 +16,47 @@ max_rad = [0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.5,6.0,6.5,7.0]
 mask = np.zeros((len(min_rad),len(b)-1))
 
 
-for k in range(len(reduc_list)):
-	cluster_list = np.array(pd.read_csv("/users/ksarmien/mmpsrc_project/reductions_lists/"+reduc_list[k],header=None))
-	median_noise = np.zeros((len(min_rad),len(cluster_list)))
-	ii=0
-	for i in cluster_list:
-		cluster = i[0][2:20]
-		c_snr =  "/home/scratch/cromero/MUSTANG2/Reductions/"+direc_list[k]+i[0][2:]
-		hdu_noise = fits.open(c_snr)[1]
-		img_noise = hdu_noise.data
-		arcsec_per_pixel = (3600*np.abs(hdu_noise.header["CD1_1"]))
-		pixel_per_arcsec = 1./arcsec_per_pixel
-		pixel_per_arcmin = pixel_per_arcsec*60.
-		arcsec_per_pixel_sq = arcsec_per_pixel**2
-		x = img_noise.shape[1]
-		y = img_noise.shape[0]
-		x_arr = np.arange(np.floor(-1*x/2),np.floor(x/2))
-		y_arr = np.arange(np.floor(-1*y/2),np.floor(y/2))
-		x_mesh,y_mesh = np.meshgrid(x_arr,y_arr)
-		r = np.sqrt(x_mesh**2+y_mesh**2)
-		for j in range(len(min_rad)):
-			n = img_noise[np.logical_and((r<=max_rad[j]*pixel_per_arcmin),(r>min_rad[j]*pixel_per_arcmin))]
-			mask[j] = mask[j]+np.cumsum(np.histogram(n,bins=b)[0])
-			med = np.median(n)
-			median_noise[j,ii] = med
-		H,x_H =np.histogram(img_noise.ravel(),bins=b)
-		H_c = np.cumsum(H)
-		H_total = H_total+H_c
-		arcsec_per_pixel_sq = (3600*np.abs(hdu_noise.header["CD1_1"]))**2
-		A_c = H_c*arcsec_per_pixel_sq
-		A_total = A_total+A_c
-		ii+=1
+cluster_list = np.array(pd.read_csv("/users/ksarmien/mmpsrc_project/reductions_lists/"+reduc,header=None))
+median_noise = np.zeros((len(min_rad),len(cluster_list)))
+ii=0
+for i in cluster_list:
+	cluster = i[0][2:20]
+	c_snr =  "/home/scratch/cromero/MUSTANG2/Reductions/"+direc+i[0][2:]
+	hdu_noise = fits.open(c_snr)[1]
+	img_noise = hdu_noise.data
+	arcsec_per_pixel = (3600*np.abs(hdu_noise.header["CD1_1"]))
+	pixel_per_arcsec = 1./arcsec_per_pixel
+	pixel_per_arcmin = pixel_per_arcsec*60.
+	arcsec_per_pixel_sq = arcsec_per_pixel**2
+	x = img_noise.shape[1]
+	y = img_noise.shape[0]
+	x_arr = np.arange(np.floor(-1*x/2),np.floor(x/2))
+	y_arr = np.arange(np.floor(-1*y/2),np.floor(y/2))
+	x_mesh,y_mesh = np.meshgrid(x_arr,y_arr)
+	r = np.sqrt(x_mesh**2+y_mesh**2)
+	for j in range(len(min_rad)):
+		n = img_noise[np.logical_and((r<=max_rad[j]*pixel_per_arcmin),(r>min_rad[j]*pixel_per_arcmin))]
+		mask[j] = mask[j]+np.cumsum(np.histogram(n,bins=b)[0])
+		med = np.median(n)
+		median_noise[j,ii] = med
+	H,x_H =np.histogram(img_noise.ravel(),bins=b)
+	H_c = np.cumsum(H)
+	H_total = H_total+H_c
+	arcsec_per_pixel_sq = (3600*np.abs(hdu_noise.header["CD1_1"]))**2
+	A_c = H_c*arcsec_per_pixel_sq
+	A_total = A_total+A_c
+	ii+=1
 
-#df = np.array(list(zip(b[1:],H_total,A_total,mask05,mask10,mask15,mask20,mask25,mask30,mask35,mask40,mask45,mask50,mask55,mask60,mask65,mask70)))
-#df = pd.DataFrame(df,columns = ["noise","cumulative_pix_num","cumulative_area_sqarcsec","mask05","mask10","mask15","mask20","mask25","mask30","mask35","mask40","mask45","mask50","mask55","mask60","mask65","mask70"])
-#df.to_csv("cumulative_noise_hist.csv")
+index_labels = list(np.str(np.array(list(zip(min_rad,max_rad)))))
+df = pd.DataFrame(mask,columns = list(np.str(b[1:])),index=index_labels)
+df.to_csv("noise_by_radii_hist.csv")
 
 fig = plt.figure()
 plt.plot(b[1:],H_total)
 plt.xlabel("Noise level")
 plt.title("Cumulative count of pixels above a noise level")
 plt.xlim(0.0,0.003)
-plt.savefig("noise_hist/cumulative_noise_pixels.png")
+plt.savefig("noise_histogram/cumulative_noise_pixels.png")
 plt.close(fig)
 
 
@@ -68,7 +67,7 @@ plt.xlabel("Noise level")
 plt.legend()
 plt.title("Cumulative histogram of pixels above a noise level in given annuli")
 plt.xlim(0.0,0.003)
-plt.savefig("noise_hist/cumulative_noise_pixels_radii.png")
+plt.savefig("noise_histogram/cumulative_noise_pixels_radii.png")
 plt.close(fig)
 
 fig = plt.figure()
@@ -78,7 +77,7 @@ for i in range(len(min_rad)-4):
 plt.title("Histogram of median noise per annuli across all cluster maps")
 plt.legend()
 plt.xlim(0.0,0.002)
-plt.savefig("noise_hist/hist_median_noise.png")
+plt.savefig("noise_histogram/hist_median_noise.png")
 plt.close(fig)
 
 for i in range(len(min_rad)-4):
@@ -92,5 +91,5 @@ for i in range(len(min_rad)-4):
 		plt.xlim(0.0,0.0005)
 	else:
 		plt.xlim(0.0,0.001)
-	plt.savefig("noise_hist/hist_median_noise"+str(max_rad[i])+".png")
+	plt.savefig("noise_histogram/hist_median_noise"+str(max_rad[i])+".png")
 	plt.close(fig)

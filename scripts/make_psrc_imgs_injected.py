3dbd83a74462f469def11d950368c9f959556798
karenperezsarmiento <kaper@sas.upenn.edu>
2023-08-03 11:57:10 -0400

    new inj psrc maps

--- scripts/make_psrc_imgs_injected.py
+++ scripts/make_psrc_imgs_injected.py
@@ -12,7 +12,7 @@ code = "20"
 theta_1 = 2.0
 theta_2 = 6.0
 fake = pd.read_csv("../psrc_lists/fake_pnt_amplitudes.csv")
-injected_found = pd.read_csv("../psrc_lists/injected_2_6_psrcs__2aspcmsubqm2_fitel_0f09-to-35f5Hz_qc_0p6rr_M_PdoCals_dt20_snr_iter1_4.0sigma.csv")
+injected_found = pd.read_csv("../psrc_lists/injected_2_10_psrcs__2aspcmsubqm2_fitel_0f09-to-35f5Hz_qc_0p6rr_M_PdoCals_dt20_snr_iter1_4.0sigma.csv")
 
 ff = np.unique(np.array(pd.read_csv("../reductions_lists/injected.txt",header=None)))
 for i in ff:
@@ -23,7 +23,7 @@ for i in ff:
 	session = i[88:-15]
 	print(c)
 	print(session)
-	fake_cluster = fake.loc[fake["cluster"]==c]
+	fake_cluster = fake.loc[fake["Cluster"]==c]
 	found_cluster = injected_found.loc[injected_found["cluster"]==c]
 	fig = plt.figure()
 	img_snr = hdu_snr.data
@@ -31,7 +31,7 @@ for i in ff:
 	plt.imshow(img_snr,origin="lower",cmap="bwr")
 	plt.title(str(c))	
 	#plt.scatter(x_first,y_first,color="none",marker="s",edgecolor="blue")
-	plt.scatter(np.array(fake_cluster["x_loc"]),np.array(fake_cluster["y_loc"]),color="none",edgecolor="green")	
+	plt.scatter(np.array(fake_cluster["X_loc_pixels"]),np.array(fake_cluster["y_loc_pixels"]),color="none",edgecolor="green")	
 	plt.scatter(np.array(found_cluster["x"]),np.array(found_cluster["y"]),color="none",edgecolor="black")    
 	plt.savefig("../psrc_img_injected/"+str(c)+"_"+str(session)+"_injected_found.png")
 	plt.close(fig)
